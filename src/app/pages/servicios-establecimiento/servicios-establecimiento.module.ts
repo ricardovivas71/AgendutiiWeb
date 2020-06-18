7fe95d47221f669c1f8c857904ed35912df351b2
luisca9310 <llopez@asesoftware.com>
2020-06-17 19:37:22 -0500

    Ajuste de módulo

--- src/app/pages/servicios-establecimiento/servicios-establecimiento.module.ts
+++ src/app/pages/servicios-establecimiento/servicios-establecimiento.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 import { ServiciosEstablecimientoPage } from './servicios-establecimiento.page';
@@ -18,6 +18,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
